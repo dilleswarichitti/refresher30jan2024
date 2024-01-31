a211fde598c8cc9c491e603057a4f2db8b6587e5
Chitti Dilleswari <2000112419@hexaware.com>
2024-01-31 12:52:33 +0530

    modified

--- React/calendar-app/src/components/Login.js
+++ React/calendar-app/src/components/Login.js
@@ -1,49 +1,137 @@
-import { useState } from "react";
-import axios from "axios";
-import './Login.css';
-import { useNavigate } from "react-router-dom";
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { Box } from '@mui/material';
 
-function Login(){
-    const [email,setEmail] = useState("");
-    const [password,setPassword] = useState("");
-    const navigate =useNavigate();
+const theme = createTheme();
+const defaultTheme = createTheme();
 
-    const login = (event)=>{
-        event.preventDefault();
-        axios.post("https://localhost:7117/api/User/Login",{
-            email: email,
-            password:password
-        })
-        .then((userData)=>{
-            var token = userData.data.token;
-            localStorage.setItem("token",token);
-            localStorage.setItem("email",email);
-            navigate("/events");
-            window.location.reload();
-        })
-        .catch((err)=>{
-            console.log(err)
-        })
-    } 
-return(
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const login = (event) => {
+    event.preventDefault();
+    axios
+      .post('https://localhost:7211/api/User/Login', {
+        email: email,
+        password: password,
+      })
+      .then((userData) => {
+        var token = userData.data.token;
+        localStorage.setItem('token', token);
+        localStorage.setItem('email', email);
+        navigate('/events');
+        window.location.reload();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
     <div className="login">
-       <div className="log">
-        <form className="login-form">
-        <h3>Login</h3>
-        <label className="form-control">Email</label>
-        <input type="email" className="form-control" value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
-        <label className="form-control">Password</label>
-        <input type="password" className="form-control" value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
-        <button className="btn btn-primary button" onClick={login}>Login</button>     
-        <button className="btn btn-danger button">Cancel</button>
-        <br/>
-        <br/>
-        <strong> New User? Register Here <a href="/">Register</a></strong>
-        </form>
-       </div> 
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <Box
+        sx={{
+            marginTop:8,
+            display:'flex',
+            flexDirection:'column',
+            alignltems:'center',
+            boxShadow:'0px 0px 10px rgba(0,0,0,0.1)',
+            borderRadius:'10px',
+            padding:'20px',
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+        }}
+        >
+        
+          <div className="log" >
+            <form className="login-form" onSubmit={handleSubmit} noValidate>
+              <Typography component="h1" variant="h5">
+                Login
+              </Typography>
+              <TextField
+                fullWidth
+                label="Email"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                margin="normal"
+                required
+                autoComplete="email"
+                autoFocus
+                sx={{ marginBottom: 1 }}
+              />
+              <TextField
+                fullWidth
+                label="Password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                margin="normal"
+                required
+                autoComplete="current-password"
+                sx={{ marginBottom: 1 }}
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+                sx={{ marginBottom: 1 }}
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                onClick={login}
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                  {"New User?"}<strong> <a href="/">Register</a></strong>
+                  </Link>
+                </Grid>
+              </Grid>
+            </form>
+          </div>
+        
+        </Box>
+      </Container>
+    </ThemeProvider>
     </div>
-);
-
+  );
 }
 
 export default Login;
--- React/calendar-app/src/components/Register.js
+++ React/calendar-app/src/components/Register.js
@@ -1,86 +1,150 @@
-import { useState } from "react";
+import * as React from 'react';
+import { useState } from 'react';
+import { Button, Avatar, CssBaseline, TextField, FormControlLabel, Checkbox, Link, Grid, Box, Typography, Container, createTheme, ThemeProvider } from '@mui/material';
+
 import './Register.css';
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
-function Register(){
-    const roles =["User"];
-    const [email,setEmail] = useState("");
-    const [firstname,setFirstName] = useState("");
-    const [lastname,setLastName] = useState("");
-    const [password,setPassword] = useState("");
-    const [role,setRole] = useState("");
-    const navigate =useNavigate();
+const theme = createTheme();
 
-    var [emailError,setEmailError]=useState("");
-    var checkUSerData = ()=>{
-        if(email=='')
-        {
-            setEmailError("Email cannot be empty");
-            return false;
-        }
-           
-        if(password=='')
-            return false;
-        return true;
+function Register() {
+  const [email, setEmail] = useState('');
+  const [firstname, setFirstName] = useState('');
+  const [lastname, setLastName] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const roles = ['User'];
+
+  const [emailError, setEmailError] = useState('');
+
+   const checkUserData = () => {
+//     if (email === '') {
+//       setEmailError('Email cannot be empty');
+//       return false;
+//     }
+
+    if (password === '') return false;
+
+    return true;
+  };
+
+  const signUp = (event) => {
+    event.preventDefault();
+    const checkData = checkUserData();
+
+    if (!checkData) {
+      alert('Please check your data');
+      return;
     }
-    const signUp = (event)=>{
-        event.preventDefault();
-        var checkData = checkUSerData();
-        if(checkData==false)
-        {
-            alert('please check yor data')
-            return;
-        }
-        
-        axios.post("https://localhost:7117/api/User",{
-            email: email,
-            firstname:firstname,
-            lastname:lastname,
-            role:"User",
-            password:password
-    })
-        .then((userData)=>{
-            var token = userData.data.token;
-            localStorage.setItem("token",token);
-            localStorage.setItem("email",email);
-            navigate("/login");
-            window.location.reload();
-        })
-        .catch((err)=>{
-            console.log(err)
-        })
-    }
-    return(
-        <div className="login">
-        <div>
-            <form className="registerForm">
-            <label className="form-control">Email</label>
-            <input type="text" className="form-control" value={email}
-                    onChange={(e)=>{setEmail(e.target.value)}}/>
-           <label className="alert alert-danger">{emailError}</label>
-           <br/>
-            <label className="form-control">Password</label>
-            <input type="password" className="form-control" value={password}
-                    onChange={(e)=>{setPassword(e.target.value)}}/>
-                     <br/>
-            <label className="form-control">FirstName</label>
-            <input type="text" className="form-control" value={firstname}
-                    onChange={(e)=>{setFirstName(e.target.value)}}/>
-             <br/>
-            <label className="form-control">LastName</label>
-            <input type="text" className="form-control" value={lastname}
-                    onChange={(e)=>{setLastName(e.target.value)}}/>
-             <br/>
-            <button className="btn btn-primary button" onClick={signUp}>Sign Up</button>
-            <button className="btn btn-danger button">Cancel</button>
-            <br/>
-            <br/>
-            <strong>Already have an account?  <a href="/Login">Login</a>.</strong>
-            </form>
-            </div>
-            </div>
-    );
+
+    axios
+      .post('https://localhost:7211/api/User', {
+        email: email,
+        firstname: firstname,
+        lastname: lastname,
+        role: 'User',
+        password: password,
+      })
+      .then((userData) => {
+        const token = userData.data.token;
+        localStorage.setItem('token', token);
+        localStorage.setItem('email', email);
+        navigate('/login');
+        window.location.reload();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div className="login">
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box component="form" className="registerForm" onSubmit={signUp} noValidate sx={{ mt: 3 }}>
+            <TextField
+              autoComplete="given-name"
+              name="firstName"
+              required
+              fullWidth
+              id="firstName"
+              label="First Name"
+              value={firstname}
+
+              onChange={(e) => setFirstName(e.target.value)}
+              sx={{ marginBottom: 1 }}
+            />
+            <TextField
+              required
+              fullWidth
+              id="lastName"
+              label="Last Name"
+              name="lastName"
+              autoComplete="family-name"
+              value={lastname}
+              onChange={(e) => setLastName(e.target.value)}
+              sx={{ marginBottom: 1 }}
+            />
+            <TextField
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              sx={{ marginBottom: 1 }}
+            />
+            <Typography className="alert alert-danger">{emailError}</Typography>
+            
+            <TextField
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="new-password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              sx={{ marginBottom: 1 }}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="/login" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+    </div>
+  );
 }
 
 export default Register;
